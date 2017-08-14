0210091159ccc130a4f403a22f43db15b9eafb55
David Nishikawa <triptype@gmail.com>
2017-08-14 16:23:18 -0700

    set up event listener to respond to votes for a set number; added vote counting capability

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var names = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweet', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-var images = ['./images/bathroom.jpg', './images/boots.jpg', './images/breakfast.jpg', './images/bubblegum.jpg', './images/chair.jpg', './images/cthulhu.jpg', './images/dog-duck.jpg', './images/dragon.jpg', './images/pen.jpg', './images/pet-sweet.jpg', './images/scissors.jpg', './images/shark.jpg', './images/sweep.png', './images/tauntaun.jpg', './images/unicorn.jpg', './images/usb.gif', './images/water-can.jpg', './images/wine-glass.jpg'];
-var descriptions = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweet', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-var ids = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweet', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var names = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var images = ['./images/bathroom.jpg', './images/boots.jpg', './images/breakfast.jpg', './images/bubblegum.jpg', './images/chair.jpg', './images/cthulhu.jpg', './images/dog-duck.jpg', './images/dragon.jpg', './images/pen.jpg', './images/pet-sweep.jpg', './images/scissors.jpg', './images/shark.jpg', './images/sweep.png', './images/tauntaun.jpg', './images/unicorn.jpg', './images/usb.gif', './images/water-can.jpg', './images/wine-glass.jpg'];
+var descriptions = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var ids = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 
 
@@ -12,6 +12,18 @@ function Product(name, image, description, id) {
     this.id = id;
     this.voted = [];
     this.shown = [];
+}
+
+Product.prototype.getVoteCount = function() {
+    return voted.length;
+}
+
+Product.prototype.getShowCount = function() {
+    return shown.length;
+}
+
+Product.prototype.getPercentVote = function() {
+    return (voted.length * shown.length) * 100 + '%';
 }
 
 
@@ -57,13 +69,6 @@ var surveyor = {
         }
     },
 
-    setListener: function() {
-        this.elVoteBox.addEventListener('click', function(e) {
-            e.target.voted.push(this.voteCount);
-            // find way to modify shown property of all options
-        }, false);
-    },
-
     canUse: function(newOptionIndex, currentOptionNum) {
         var unavailableIndices = this.usedOptionIndices.slice( -( (2*this.numOptions) + currentOptionNum));
         for(var i = 0; i < unavailableIndices.length; i++) {
@@ -84,23 +89,52 @@ var surveyor = {
             }
         } while(i < this.numOptions);
         var newRandomIndices = this.usedOptionIndices.slice(-3);
+        console.log('in getRandomIndices; usedOptionIndices=' + this.usedOptionIndices);
+        console.log('in getRandomIndices; newRandomIndices=' + newRandomIndices);
         return newRandomIndices;
     },
 
     renderProduct: function(productIndex, optionIndex) {
+        var elImage = document.createElement('img');
+        elImage.setAttribute('data-index', productIndex);
+        elImage.setAttribute('src', this.products[productIndex].image);
+        elImage.setAttribute('alt', this.products[productIndex].description);
+        elImage.setAttribute('id', this.products[productIndex].id);
+        this.elOptionImages[optionIndex].innerHTML = '';
+        this.elOptionImages[optionIndex].appendChild(elImage);
 
+        this.elOptionDescriptions[optionIndex].innerText = this.products[productIndex].description;
     },
 
-    showOptions: function() {
-        
+    renderOptions: function() {
+        var randomProductSet = this.getRandomIndices();
+        for(var i = 0; i < this.numOptions; i++) {
+            this.renderProduct(randomProductSet[i], i);
+        }
+    },
+
+    vote: function(e) {
+        surveyor.products[parseInt(e.target.getAttribute('data-index'))].voted.push(surveyor.voteCount);
+        console.log(surveyor.voteCount);
+        if(surveyor.voteCount < surveyor.numVotes) {
+            surveyor.voteCount++;
+            surveyor.renderOptions();
+        }
+        else {
+            console.log('survey over');
+            surveyor.elVoteBox.removeEventListener('click', surveyor.vote, false);
+        }
+    },
+
+    setListener: function() {
+        this.elVoteBox.addEventListener('click', surveyor.vote, false);
     },
 
     survey: function() {
         this.populateElementVariables();
+        this.instantiateProducts();
         this.setListener();
-        for(voteCount = 0; voteCount < this.numVotes; voteCount++) {
-            // this.showOptions();
-        }
+        this.renderOptions();
         // this.removeListener();
     }
 }
