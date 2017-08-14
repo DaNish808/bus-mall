d4e1212df584a0676a525185752be9ee73a9997a
David Nishikawa <triptype@gmail.com>
2017-08-14 15:23:25 -0700

    created surveyor methods to instantiate products and generate non-repeating sets of indices

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
+var names = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweet', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var images = ['./images/bathroom.jpg', './images/boots.jpg', './images/breakfast.jpg', './images/bubblegum.jpg', './images/chair.jpg', './images/cthulhu.jpg', './images/dog-duck.jpg', './images/dragon.jpg', './images/pen.jpg', './images/pet-sweet.jpg', './images/scissors.jpg', './images/shark.jpg', './images/sweep.png', './images/tauntaun.jpg', './images/unicorn.jpg', './images/usb.gif', './images/water-can.jpg', './images/wine-glass.jpg'];
+var descriptions = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweet', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var ids = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweet', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+
+
 function Product(name, image, description, id) {
     this.name = name;
     this.image = image;
     this.description = description;
     this.id = id;
-    this.voteCount = [];
-    this.showCount = [];
+    this.voted = [];
+    this.shown = [];
 }
 
 
-
-var survey = {
+var surveyor = {
     products: [],
+    usedOptionIndices: [],
     numOptions: 3,
-    voteNum: 0,
+    numVotes: 3,
+    voteCount: 0,
     elVoteBox: document.getElementById('vote-box'),
     elOptionImages: [],
     elOptionDescriptions: [],
@@ -41,5 +49,60 @@ var survey = {
         for(var i = 0; i < this.numOptions; i++) {
             this.setOptionElement(i);
         }
+    },
+
+    instantiateProducts: function() {
+        for(var i = 0; i < names.length; i++) {
+            this.products[i] = new Product (names[i], images[i], descriptions[i], ids[i]);
+        }
+    },
+
+    setListener: function() {
+        this.elVoteBox.addEventListener('click', function(e) {
+            e.target.voted.push(this.voteCount);
+            // find way to modify shown property of all options
+        }, false);
+    },
+
+    canUse: function(newOptionIndex, currentOptionNum) {
+        var unavailableIndices = this.usedOptionIndices.slice( -( (2*this.numOptions) + currentOptionNum));
+        for(var i = 0; i < unavailableIndices.length; i++) {
+            if(newOptionIndex === unavailableIndices[i]) {
+                return false;
+            }
+        }
+        return true;
+    },
+
+    getRandomIndices: function() {
+        var i = 0;
+        do {
+            var newOptionIndex = Math.floor(Math.random() * this.products.length);
+            if(this.canUse(newOptionIndex, i + 1)) {
+                this.usedOptionIndices.push(newOptionIndex);
+                i++;
+            }
+        } while(i < this.numOptions);
+        var newRandomIndices = this.usedOptionIndices.slice(-3);
+        return newRandomIndices;
+    },
+
+    renderProduct: function(productIndex, optionIndex) {
+
+    },
+
+    showOptions: function() {
+        
+    },
+
+    survey: function() {
+        this.populateElementVariables();
+        this.setListener();
+        for(voteCount = 0; voteCount < this.numVotes; voteCount++) {
+            // this.showOptions();
+        }
+        // this.removeListener();
     }
 }
+
+surveyor.survey();
