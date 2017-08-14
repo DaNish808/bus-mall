34632e1fc8712d9dfa8e46e1d435c56485de4a61
David Nishikawa <triptype@gmail.com>
2017-08-14 13:48:33 -0700

    set up product constructor and survey object literal; added method to survey object to dynamically populate vote box

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-alert('js online');
+function Product(name, image, description, id) {
+    this.name = name;
+    this.image = image;
+    this.description = description;
+    this.id = id;
+    this.voteCount = [];
+    this.showCount = [];
+}
+
+
+
+var survey = {
+    products: [],
+    numOptions: 3,
+    voteNum: 0,
+    elVoteBox: document.getElementById('vote-box'),
+    elOptionImages: [],
+    elOptionDescriptions: [],
+
+    setOptionElement: function(index) {
+        var elOptionBox = document.createElement('div');
+        elOptionBox.setAttribute('id', 'option' + index);
+        elOptionBox.setAttribute('class', 'vote-option');
+
+        var elImageDiv = document.createElement('div');
+        elImageDiv.setAttribute('id', 'option' + index + '-image');
+        elImageDiv.setAttribute('class', 'option-image');
+        this.elOptionImages.push(elImageDiv);
+        elOptionBox.appendChild(elImageDiv);
+
+        var elDescription = document.createElement('div');
+        elDescription.setAttribute('id', 'option' + index + '-description');
+        elDescription.setAttribute('class', 'option-description');
+        this.elOptionDescriptions.push(elDescription);
+        elOptionBox.appendChild(elDescription);
+
+        this.elVoteBox.appendChild(elOptionBox);
+    },
+
+    populateElementVariables: function() {
+        for(var i = 0; i < this.numOptions; i++) {
+            this.setOptionElement(i);
+        }
+    }
+}
