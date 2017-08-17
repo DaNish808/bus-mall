b8ef99496867d96c2b4ea87587ec5b62fe73a941
David Nishikawa <triptype@gmail.com>
2017-08-17 09:49:30 -0700

    set up local storage system; created chart for the sum of all past results

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const ids = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 
 
 
 
+
 function Product(name, image, description, id) {
     this.name = name;
     this.image = image;
@@ -28,20 +29,30 @@ Product.prototype.getPercentVote = function() {
     }
     else {
         var rawPercentage = (this.getVoteCount() / this.getShowCount()) * 100;
-        var roundedPercentage = Math.floor(rawPercentage * 100) /100 + '%';
+        var roundedPercentage = Math.floor(rawPercentage * 100) /100;
         return roundedPercentage;
     }
 }
 
 
+
+
 var surveyor = {
+    participant: 'hi',
+    allParticipants: [],
     products: [],
     usedOptionIndices: [],      // logs every displayed product in order
     numOptions: 3,      // sets number of options displayed per vote
     numVotes: 25,      // sets number of votes per survey
     voteCount: 0,       // iterates with each vote until (voteCount === numVotes)
 
-    results: [ ['Product', 'Votes', 'Shown', 'Percentage'] ],       // temporary array for tablulating voting results in console (logResults method)
+    data: {
+        grossResults: [], // stores all past survey data
+        results: [ ['Product', 'Votes', 'Shown', 'Percentage'] ],       // temporary array for tablulating voting results in console (logResults method)
+        allVoteResults: [],
+        allShownResults: [],
+        allPercentageResults: [],
+    },
 
     elVoteBox: document.getElementById('vote-box'),     // parent container of image options
     elOptionImages: [],         // elOptionImages and elOptionDescritions are populated in the setOptionElement method
@@ -70,7 +81,33 @@ var surveyor = {
         }
     },
 
+    elResultsChartAll: document.getElementById('all-results-chart'),
+    chartDataAll: {
+        type: 'bar',
+        data: {
+            labels: names,
+            datasets: [{
+                label: 'participant',
+                data: [],
+                backgroundColor: 'darkblue',
+            }],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    },
+
+
+
     elResultsBox: document.getElementById('results-box'),
+
+
 
     setOptionElement: function(index) {                     // sets up the elements for one option including:
         var elOptionBox = document.createElement('div');        // container for image and description
@@ -154,6 +191,55 @@ var surveyor = {
         }
     },
 
+
+    // must be done after calculating data.results
+    // takes "grossResults" from local storage and parses it into data.grossResults
+    dataFromLocalStorage: function() {
+        var dataString = localStorage.getItem('grossResults');
+        if(dataString) {
+            this.data.grossResults = JSON.parse(dataString);
+        }
+    },
+    
+    // stores data.results and updates data.grossResults in local storage
+    dataToLocalStorage: function() {
+        
+        // use dataString to store results
+        var dataString = JSON.stringify(this.data.results);
+        localStorage.setItem('results', dataString);
+
+        // use dataString to store grossResults
+        this.data.grossResults.push(this.data.results);
+        dataString = JSON.stringify(this.data.grossResults);
+        localStorage.setItem('grossResults', dataString);
+
+    },
+
+    // sorts through data from grossResults to get summary totals
+    // stored data in this.data.all* properties
+    getAllResults: function() {
+        var allVotes = [],
+            allShown = [],
+            allPercentages = [];
+
+        for(var k = 0; k < (this.data.results.length - 1); k++) {
+            allVotes[k] = 0;
+            allShown[k] = 0;
+            allPercentages[k] = 0;
+        }
+
+        for(var i = 0; i < this.data.grossResults.length; i++) {
+            for(var j = 1; j < this.data.grossResults[i].length; j++) {
+                allVotes[j - 1] += this.data.grossResults[i][j][1];
+                allShown[j - 1] += this.data.grossResults[i][j][2];
+                allPercentages[j - 1] = ((allPercentages * i) + this.data.grossResults[i][j][3]) / (i+1);
+            }
+        }
+        this.data.allVoteResults.push(allVotes);
+        this.data.allShownResults.push(allShown);
+        this.data.allPercentageResults.push(allPercentages);
+    },
+
     calcProductsShown: function() {     // called after all votes have been cast
                                         // for each option that was shown, push the voteCount that it occurred on to that product's shown property
                                         // the length of productName.shown is used by its getShowCount() method/
@@ -166,10 +252,14 @@ var surveyor = {
         document.getElementById('voting-header').setAttribute('class', 'hidden');
         document.getElementById('results-header').removeAttribute('class');
         document.getElementById('canvas').removeAttribute('class');
+        document.getElementById('canvas-all').removeAttribute('class');
         document.getElementById('canvasTest').setAttribute('class', 'hidden');
+        document.getElementById('animation-controls').setAttribute('class', 'hidden');
+        document.getElementById('for-dev').setAttribute('class', 'hidden');
         clearInterval(animation);
         surveyor.elVoteBox.setAttribute('class', 'hidden');
         surveyor.elResultsChart.removeAttribute('class');
+        surveyor.elResultsChartAll.removeAttribute('class');
         surveyor.elResultsBox.removeAttribute('class');
     },
 
@@ -177,19 +267,19 @@ var surveyor = {
         var elTable = document.createElement('table');
 
             var elThead = document.createElement('thead');
-                for(var i = 0; i < surveyor.results[0].length; i++) {
+                for(var i = 0; i < surveyor.data.results[0].length; i++) {
                     var elTh = document.createElement('th');
-                        elTh.innerText = surveyor.results[0][i];
+                        elTh.innerText = surveyor.data.results[0][i];
                     elThead.appendChild(elTh);
                 }
             elTable.appendChild(elThead);
 
             for(var i = 1; i <= names.length; i++) {
                 var elTr = document.createElement('tr');
-                    for(var j = 0; j < surveyor.results[0].length; j++) {
+                    for(var j = 0; j < surveyor.data.results[0].length; j++) {
                         var elTd = document.createElement('td');
                             if(j < 1) {
-                                elTd.innerText = surveyor.results[i][j];
+                                elTd.innerText = surveyor.data.results[i][j];
                                 elTd.setAttribute('class', 'popup-image')
                                 var elImg = document.createElement('img');
                                     elImg.setAttribute('src', surveyor.products[i-1].image);
@@ -197,7 +287,7 @@ var surveyor = {
                                 elTd.appendChild(elImg);
                             }
                             else {
-                                elTd.innerText = surveyor.results[i][j];
+                                elTd.innerText = surveyor.data.results[i][j];
                             }
                         elTr.appendChild(elTd);
                     }
@@ -208,16 +298,30 @@ var surveyor = {
         surveyor.elResultsBox.appendChild(elTable);
     },
 
-    calcChartData: function() {
-        for(var i = 1; i <= names.length; i++) {
-            this.chartData.data.datasets[0].data.push(this.results[i][1]);
+    calcChartData: function(type) {
+        if(type === 'current') {
+            this.chartData.data.datasets[0].label = this.participant;
+            for(var i = 1; i <= names.length; i++) {
+                this.chartData.data.datasets[0].data.push(this.data.results[i][1]);
+            }
+        }
+        else if(type === 'all') {
+            this.chartDataAll.data.datasets[0].label = 'Sum of All Previous Surveys';
+            for(var i = 0; i <= names.length; i++) {
+                this.chartDataAll.data.datasets[0].data.push(this.data.allVoteResults[0][i]);
+            }
         }
     },
 
     renderChart: function() {
         var elCanvas = document.getElementById('canvas');
-        this.calcChartData();
+        this.calcChartData('current');
         var productsChart = new Chart(elCanvas, this.chartData);
+
+        
+        var elCanvasAll = document.getElementById('canvas-all');
+        this.calcChartData('all');
+        var productsChart = new Chart(elCanvasAll, this.chartDataAll);
     },
 
     logResults: function(e) {      // temporarily displays survey results to console
@@ -227,10 +331,12 @@ var surveyor = {
             productData.push(surveyor.products[i].getVoteCount());
             productData.push(surveyor.products[i].getShowCount());
             productData.push(surveyor.products[i].getPercentVote());
-            surveyor.results.push(productData);
+            surveyor.data.results.push(productData);
         }
-        console.table(surveyor.results);
+        // console.table(surveyor.data.results);
     },
+
+
 
     vote: function(e) {     // fires when user clicks an image
         // pushes the current voteCount onto the clicked product's voted log.
@@ -260,12 +366,15 @@ var surveyor = {
     },
 
     survey: function() {                   // initializes survey by 
+        this.participant = prompt('Hi! What\'s your name?');
         this.voteCount = 0;                // restarting the vote counter
         this.populateElementVariables();   // creating and assigning the necessary elements to properties in the survey object
         this.instantiateProducts();        // creating the product objects in the .products property
         this.setListener();                // setting up the 'click' event listener
         this.renderOptions();              // and rendering the first set of voting options to the page
     },
+
+
 
     testSurvey: function(iterations) {     // fires when user clicks an image
         for(iterations; iterations > 0; iterations--) {
@@ -282,6 +391,9 @@ var surveyor = {
         surveyor.switchLayout();                        // removes vote-box and adds results-box
         surveyor.logResults();                         // and show results
         surveyor.renderResults();
+        surveyor.dataFromLocalStorage();
+        surveyor.dataToLocalStorage();
+        surveyor.getAllResults();
         surveyor.renderChart();
     },
 
@@ -294,6 +406,8 @@ var surveyor = {
         });
     }
 }
+
+
 
 surveyor.survey();      
 surveyor.listenAutoRun();
@@ -310,7 +424,21 @@ surveyor.listenAutoRun();
 
 
 
+
+
+
+
+
+
+
+
 // canvas is   800 x 300
+// <canvas id='canvasTest' height='300' width='800'></canvas>
+// <div id='animation-controls'>
+//     <button id='startAnimation'>play</button>
+//     <button id='endAnimation'>pause</button>
+// </div>
+
 
 var elCanvas = document.getElementById('canvasTest');
 var context = elCanvas.getContext('2d');
@@ -476,6 +604,7 @@ function mainDraw() {
 
 // var animation = setInterval(mainDraw, 80);
 // setTimeout(setInterval(mainDraw, 5), 10000000);
+
 
 
 
